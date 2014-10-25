bb991566e36ff09c59af9f2154265624b040b32d
Robert Forgione <rforgione@gmail.com>
2014-10-25 17:16:42 -0400

    Added data scrub functionality to import Peyton's regular season data; Added functionality to skip over the bottom subtotal lines in each data set; added a toggle into the arguments in the data_scrub function that allows for distinguishing between scrubbing playoff and regular season data'

--- data_scrub.py
+++ data_scrub.py
@@ -4,20 +4,36 @@ import pandas as pd
 import numpy as np
 
 
-def data_scrub():
+def data_scrub(playoffs=False):
+	# regex patterns denoting subtotal and column title lines
+	# to be excluded from the data set
 	p1 = re.compile('^Passing')
 	p2 = re.compile('^Rk')
+	p3 = re.compile('^(\d+ Games)')
 	pdata = pd.DataFrame()
 
-	with open('peyton_playoffs2.csv', 'rb') as csvfile:
+	if playoffs:
+		datafile = 'peyton_playoffs2.csv'
+	else:
+		datafile = 'peyton_reg.csv'
+
+	with open(datafile, 'rb') as csvfile:
 		data = csv.reader(csvfile)
 		for row in data:
-			if not p1.search(''.join(row)) and not p2.search(''.join(row)):
+			if not p1.search(''.join(row)) and not p2.search(''.join(row)) and \
+													not p3.search(''.join(row)):
 				pdata = pdata.append(pd.Series(row), ignore_index=True)
-
-	pdata.columns = ['Rk', 'Year', 'GNum', 'Date', 'Age', 'Tm', 'At',
-					 'Opp', 'Result', 'GS', 'Cmp', 'Att', 'CmpPct', 'Yds',
-					 'TD', 'Int', 'Rate', 'YA', 'AYA', 'RushAtt', 'RushYds', 'RushYA',
-					 'RushTD', 'RushTD2','RushPTS']
+	
+	if playoffs:
+		pdata.columns = ['Rk', 'Year', 'GNum', 'Date', 'Age', 'Tm', 'At',
+						 'Opp', 'Result', 'GS', 'Cmp', 'Att', 'CmpPct', 'Yds',
+						 'TD', 'Int', 'Rate', 'YA', 'AYA', 'RushAtt', 'RushYds', 'RushYA',
+						 'RushTD', 'RushTD2','RushPTS']
+	else:
+		pdata.columns = ['Rk', 'Year', 'GNum', 'Date', 'Age', 'Tm', 'At',
+						 'Opp', 'Result', 'GS', 'Cmp', 'Att', 'CmpPct', 'Yds',
+						 'TD', 'Int', 'Rate', 'YA', 'AYA', 'RushAtt', 'RushYds', 'RushYA',
+						 'RushTD', 'Rec', 'Tgt', 'Yds', 'YR', 'RecTD', 'TotTD',
+						 'TotPts', 'Sk', 'Tkl', 'Ast']
 	return pdata
 
